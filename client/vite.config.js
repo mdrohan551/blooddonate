ad92658681a8d86cae5fb88a7ae2428dd7d476b3
Your Name <armanhossain0175019@gmail.com>
2025-03-21 22:31:11 +0600

    new api backend

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      '/api/':{
-        target:'http://localhost:2020'
+  server: {
+    proxy: {
+      '/api/v1': {
+        target: 'http://oneblood.azimemil.xyz',
+        changeOrigin: true,
+        secure: false,
       }
     }
   }
