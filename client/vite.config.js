c241fda14ac147e17e7995318bac8ca9a8101cdf
Your Name <armanhossain0175019@gmail.com>
2025-03-24 02:18:37 +0600

    no update

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,10 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // '/api/v1': {
-      '/api/': {
-        // target: 'http://oneblood.azimemil.xyz',
-        target: 'http://localhost:2020',
+      '/api/v1': {
+        target: 'http://oneblood.azimemil.xyz',
         changeOrigin: true,
         secure: false,
       }
